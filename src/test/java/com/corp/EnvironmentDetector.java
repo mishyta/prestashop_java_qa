544d537ba33913ea5720136a3f1a4e29dec5868d
mishyta <mojpizduk@gmail.com>
2021-11-18 16:21:52 +0200

    test environment

--- src/test/java/com/corp/EnvironmentDetector.java
+++ src/test/java/com/corp/EnvironmentDetector.java
@@ -1,24 +1,12 @@
 package com.corp;
 
-
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class EnvironmentDetector {
 
     static final boolean JNKENV = detectJenkins();
 
-    public static void main(String[] args) {
-        detectJenkins();
+    private static boolean detectJenkins() {
+        return System.getenv().toString().toLowerCase().contains("jenkins");
     }
 
-    private static boolean detectJenkins() {
-        AtomicBoolean res = new AtomicBoolean(false);
-        System.getenv().forEach((k, v) -> {
-            if(k.toLowerCase().contains("jenkins")){
-                res.set(true);
-            }
-        });
-        return res.get();
-    }
 }
 
